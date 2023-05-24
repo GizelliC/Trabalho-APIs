16b8854ca5d5bce6257e454ac947dd48b5762197
GizelliC <gorete3036@gmail.com>
2023-05-24 13:46:58 -0300

    Atualização1

--- script.js
+++ script.js
@@ -4,23 +4,18 @@ formulario.addEventListener('submit', function(e) {
 
     // bloqueia o reflesh da página
     e.preventDefault()
-    // Url da pesquisa
-    let urlform = "https://dog.ceo/api/breeds/image/random Fetch!";
-
+   
     //valor do input name
-    let nome = document.getElementById("name")
+    let nome = document.getElementById("name");
 
-    //concatena a url com o inputname
-    urlform = urlform + this.name.value
+    // Url da pesquisa
+    let urlform = "https://dog.ceo/api/breed/"+nome.value+"/images";
 
-    // transforma os valores em minúsculas
-    urlform = urlform.toLocaleLowerCase()
-
-    // ID content
-    let respota = document.getElementById('content')
+     // ID content
+    let respota = document.getElementById('content');
 
     //ID imgcachorro
-    let imagem = document.getElementById('imgcachorro')
+    let imagem = document.getElementById('imgcachorro');
 
     // Resposta em HTML
     let html = ''
@@ -28,14 +23,13 @@ formulario.addEventListener('submit', function(e) {
     fetch(urlform)
         .then(respota => respota.json())
         .then(function(data){
-            console.log(data)
-            html = 'Nome:' + data.name
-            respota.innerHTML = html
+            console.log(data);
+            imagem.src =  data.message[0];
+            html = 'Nome:' + raca;
+            respota.innerHTML = html;
         })
         .catch(function(err){
             console.log(err)
         })
-
-    
     });
     
