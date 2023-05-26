948e52c69fd4bdcc53a0911ce82951ac1fe5c611
GizelliC <gorete3036@gmail.com>
2023-05-26 09:19:58 -0300

    feito o localstore

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
-var formulario = document.querySelector('form')
+const formulario = document.querySelector('form');
+const botaoAdicionar = document.getElementById("btn-adicionar");
+const imagem = document.getElementById('imgcachorro');
+const nome = document.getElementById("name");
+const contextoLista = document.getElementById("contexto-lista");
+
+
+
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+//inserir dados
+
+exibirItensNaTela();
+
+botaoAdicionar.addEventListener("click", (e) => {
+    e.preventDefault();
+    adicionarCachorroNaLista();
+})
+
+function adicionarCachorroNaLista(){
+
+    // pegar a raça do cachorro
+    let nomeCachorro = nome.value;
+    // pegar a URL
+    let enderecoImagem = imagem.src;
+
+    // gravar numa lista
+
+    itens.push({
+        id: new Date(),
+        nome: nomeCachorro,
+        imagem: enderecoImagem
+    });
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    exibirItensNaTela();
+}
+
+function exibirItensNaTela(){
+
+    contextoLista.innerHTML = "";
+    itens.forEach(item => {
+        adicionaItemNaTela(item);
+    });
+}
+
+function adicionaItemNaTela(item){
+    contextoLista.innerHTML += criarItemLista(item);
+}
+
+
+function criarItemLista(item){
+
+    return `  <div class="item" >
+                    <img height="100px" max-width="100px" id="imgcachorro" src="${item.imagem}">
+                    ${item.nome} 
+                    <a data-id="${item.id}" >X</a>
+                </div>`;
+
+}
 
 formulario.addEventListener('submit', function(e) {
 
     // bloqueia o reflesh da página
-    e.preventDefault()
+    e.preventDefault();
    
-    //valor do input name
-    let nome = document.getElementById("name");
-
     // Url da pesquisa
     let urlform = "https://dog.ceo/api/breed/"+nome.value+"/images";
 
@@ -15,7 +72,7 @@ formulario.addEventListener('submit', function(e) {
     let respota = document.getElementById('content');
 
     //ID imgcachorro
-    let imagem = document.getElementById('imgcachorro');
+    
 
     // Resposta em HTML
     let html = ''
